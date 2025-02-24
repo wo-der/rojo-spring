1a81a8db7ee741e9b73bb6db1eed629ae32a9e6c
wo-der <hyungtargee@gmail.com>
2025-02-25 07:07:54 +0900

    Remove Generics

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,16 @@
-declare class Spring<M extends number, F extends number, D extends number, S extends number> {
+declare class Spring {
 	public Target: Vector3;
 	public Velocity: Vector3;
 	public Position: Vector3;
 
-	public Mass: M;
-	public Force: F;
-	public Damping: D;
-	public Speed: S;
+	public Mass: number;
+	public Force: number;
+	public Damping: number;
+	public Speed: number;
 
-	public constructor(mass?: M, force?: F, damping?: D, speed?: S);
+	public constructor(mass?: number, force?: number, damping?: number, speed?: number);
 
-	public GetStats(): LuaTuple<[M, F, D, S]>;
+	public GetStats(): LuaTuple<[number, number, number, number]>;
 	public ChangeStats(mass?: number, force?: number, damping?: number, speed?: number): void;
 	public Shove(force: Vector3): Vector3;
 	public Update(deltaTime: number): Vector3;
